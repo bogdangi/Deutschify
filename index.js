90a9d8aab750ddf923fa7852ecc41a940f9aa96e
Bogdan Girman <bogdan.girman@gmail.com>
2026-09-11 08:11:21 +0200

    Second commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPnhJpmz7-8RpvKvKl4RQtb8_iw9oCFCc",
+  authDomain: "loops-793a8.firebaseapp.com",
+  databaseURL: "https://loops-793a8.firebaseio.com",
+  projectId: "loops-793a8",
+  storageBucket: "loops-793a8.firebasestorage.app",
+  messagingSenderId: "982706291539",
+  appId: "1:982706291539:web:bdeca4df7de8add956d7a6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
